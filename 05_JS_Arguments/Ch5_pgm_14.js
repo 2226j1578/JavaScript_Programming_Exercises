b738f9d630a83b7e8b2ef52992ae57dbb4c0fa53
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 21:16:11 +0530

    Update Ch5_pgm_14.js

--- 05_JS_Arguments/Ch5_pgm_14.js
+++ 05_JS_Arguments/Ch5_pgm_14.js
@@ -73,3 +73,61 @@ showPlayerInfo(player2.name, player2.place, player2.health);
  *    **********
  *
  */
+
+var showPlayerName = function (playerName) {
+    console.log(playerName);
+};
+
+var showPlayerHealth = function (playerName, playerHealth) {
+    console.log(playerName + " has health " + playerHealth);
+};
+
+var showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName + " is in " + playerPlace);
+};
+
+// Function to show a line of specified length
+var showLine = function (length) {
+    var line = "****************************"; // Long string of asterisks
+    console.log(line.substring(0, length));
+};
+
+// Function to show a blank line
+var showBlankLine = function () {
+    console.log("");
+};
+
+// Updated showPlayerInfo function
+var showPlayerInfo = function (playerName, playerPlace, playerHealth) {
+    showBlankLine();
+
+    var nameLineLength = playerName.length + 4; // Length of the name line including the spaces and asterisks
+    showLine(nameLineLength);
+    console.log("* " + playerName + " *");
+    showLine(nameLineLength);
+
+    showBlankLine();
+
+    showPlayerPlace(playerName, playerPlace);
+    showPlayerHealth(playerName, playerHealth);
+
+    showBlankLine();
+};
+
+// Player objects
+var player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
+
+var player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+// Display player info
+showPlayerInfo(player1.name, player1.place, player1.health);
+showPlayerInfo(player2.name, player2.place, player2.health);
+
