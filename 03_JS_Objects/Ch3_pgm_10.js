c7ff60233e318ea811accfd5ccf9ad6f160ce7b3
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:33:45 +0530

    Update Ch3_pgm_10.js

--- 03_JS_Objects/Ch3_pgm_10.js
+++ 03_JS_Objects/Ch3_pgm_10.js
@@ -26,3 +26,18 @@ player1.score = 50;
  * 3) Update other properties using the console.
  *
  */
+
+
+var player1;
+
+player1 = {
+    name: "Max",
+    attempted: 0,
+    correct: 0
+};
+
+player1.attempted = 1;
+player1.correct = 1;
+player1.score = 100;
+
+console.log(player1); // Output the initial state of player1 object
