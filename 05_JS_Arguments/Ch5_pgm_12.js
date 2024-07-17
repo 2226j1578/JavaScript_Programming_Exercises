2dff7f44c67df6466b3809873bbe88d2dc8b670f
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 21:14:52 +0530

    Update Ch5_pgm_12.js

--- 05_JS_Arguments/Ch5_pgm_12.js
+++ 05_JS_Arguments/Ch5_pgm_12.js
@@ -42,3 +42,33 @@ showPlayerPlace(player2.name, player2.place);
  *    to the substring function have?
  *
  */
+
+var player1;
+var player2;
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName + " is in " + playerPlace);
+};
+
+player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
+
+player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName.substring(0, 3) + " is in " + playerPlace);
+};
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
