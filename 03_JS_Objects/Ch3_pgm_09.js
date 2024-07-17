2a9289cdc6301172c6a40caf9d677042fec64cb5
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:32:06 +0530

    Update Ch3_pgm_09.js

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -30,3 +30,40 @@ console.log(book2.title + " by " + book2.author);
  *    new property.
  *
  */
+
+
+
+var book1;
+var book2;
+
+book1 = {
+	title: "The Hobbit",
+	author: "J. R. R. Tolkien"
+};
+
+book2 = {
+	title: "Northern Lights",
+	author: "Philip Pullman"
+};
+
+console.log(book1.title + " by " + book1.author); // Logs details of book1
+console.log(book2.title + " by " + book2.author); // Logs details of book2
+
+// Further Adventures
+// 1) Add a third book.
+var book3 = {
+	title: "To Kill a Mockingbird",
+	author: "Harper Lee"
+};
+
+// 2) Log its details to the console.
+console.log(book3.title + " by " + book3.author); // Logs details of book3
+
+// 3) Add a third property to book3.
+book3.year = 1960;
+
+// 4) Update the messages logged to include the new property.
+console.log(book1.title + " by " + book1.author + ", published in " + book1.year);
+console.log(book2.title + " by " + book2.author + ", published in " + book2.year);
+console.log(book3.title + " by " + book3.author + ", published in " + book3.year);
+
