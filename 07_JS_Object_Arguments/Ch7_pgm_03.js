b03c697a9dace6a1a98008f50aa485b7791a1ffa
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 21:30:29 +0530

    Update Ch7_pgm_03.js

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -33,3 +33,53 @@ console.log(getPlanetInfo(planet2));
  *    display objects that represent cars.
  *
  */
+
+var buildPlanet;
+var getPlanetInfo;
+var planet1;
+var planet2;
+
+buildPlanet = function (name, position, type, radius, rank) {
+    return {
+        name: name,
+        position: position,
+        type: type,
+        radius: radius,
+        sizeRank: rank
+    };
+};
+
+getPlanetInfo = function (planet) {
+    return planet.name.toUpperCase() + ": planet " + planet.position;
+};
+
+planet1 = buildPlanet("Jupiter", 5, "Gas Giant", 69911, 1);
+planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
+
+console.log(getPlanetInfo(planet1));
+console.log(getPlanetInfo(planet2));
+
+var buildCar;
+var getCarInfo;
+var car1;
+var car2;
+
+buildCar = function (make, model, year, color, type) {
+    return {
+        make: make,
+        model: model,
+        year: year,
+        color: color,
+        type: type
+    };
+};
+
+getCarInfo = function (car) {
+    return car.make.toUpperCase() + " " + car.model + ": " + car.year + " (" + car.type + ")";
+};
+
+car1 = buildCar("Toyota", "Camry", 2020, "Blue", "Sedan");
+car2 = buildCar("Tesla", "Model S", 2022, "Red", "Electric");
+
+console.log(getCarInfo(car1));
+console.log(getCarInfo(car2));
