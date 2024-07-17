44751bf6eb8efb409bbfc6518fa96bfca7fbdcf3
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:41:42 +0530

    Update Ch4_pgm_05.js

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -31,3 +31,19 @@ displayMenu = function () {
  * 5) Run the displayMenu function.
  *
  */
+
+var findTotal;
+var displayMenu;
+
+findTotal = function () {
+	result = number1 + number2;
+};
+
+displayMenu = function () {
+	console.log("Please choose an option:");
+	console.log("(1) Print log");
+	console.log("(2) Upload file");
+	console.log("(9) Quit");
+};
+
+var number1, number2, result;
