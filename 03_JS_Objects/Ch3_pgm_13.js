dc5ea1467f50c461cb7ad3191655d8cadac4fca8
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:35:56 +0530

    Update Ch3_pgm_13.js

--- 03_JS_Objects/Ch3_pgm_13.js
+++ 03_JS_Objects/Ch3_pgm_13.js
@@ -39,3 +39,17 @@ var location = {
    *    instead of location.
    *
    */
+
+
+var location = {
+    "city"      : "San Francisco",
+    "state"     : "CA",
+    "country"   : "US",
+    "zip"       : "94101",
+    "latitude"  : 37.775,
+    "longitude" : -122.418,
+    "elevation" : 47.000
+  };
+  console.log(location.city + " in " + location.state + " in " + location.country);
+  
+  
