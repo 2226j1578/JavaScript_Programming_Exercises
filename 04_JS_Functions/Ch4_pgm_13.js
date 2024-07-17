135c64564eac84204d89b93525b958c803acf8ba
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:48:22 +0530

    Update Ch4_pgm_13.js

--- 04_JS_Functions/Ch4_pgm_13.js
+++ 04_JS_Functions/Ch4_pgm_13.js
@@ -46,3 +46,44 @@ showPlayerInfo();
  *    to use your two functions.
  *
  */
+
+var player1;
+var player2;
+var player;
+var showPlayerInfo;
+
+player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
+
+player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+showPlayerInfo = function () {
+    console.log(player.name);
+    console.log("------------------------------");
+    console.log(player.name + " is in " + player.place);
+    console.log(player.name + " has health " + player.health);
+    console.log("------------------------------");
+    console.log("");
+};
+
+player = player1;
+showPlayerInfo();
+
+player = player2;
+showPlayerInfo();
+
+var showPlayerInfo = function () {
+    console.log(player.name);
+    console.log("------------------------------");
+    showPlayerPlace(player);
+    showPlayerHealth(player);
+    console.log("------------------------------");
+    console.log("");
+};
