6446bded7d2ec16398035c75eef7c40bbc030ea9
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 21:29:02 +0530

    Update Ch7_pgm_01.js

--- 07_JS_Object_Arguments/Ch7_pgm_01.js
+++ 07_JS_Object_Arguments/Ch7_pgm_01.js
@@ -31,3 +31,37 @@ console.log(getPlanetInfo(planet1));
  *    each planet.
  *
  */
+
+var planet1, planet2;
+var getPlanetInfo;
+
+planet1 = {
+    name: "Jupiter",
+    position: 5,
+    type: "Gas Giant",
+    radius: 69911,
+    sizeRank: 1
+};
+
+// Creating a second planet object
+planet2 = {
+    name: "Saturn",
+    position: 6,
+    type: "Gas Giant",
+    radius: 58232,
+    sizeRank: 2
+};
+
+// Updated getPlanetInfo function to include more information
+getPlanetInfo = function (planet) {
+    return planet.name + ": planet number " + planet.position +
+           "\nType: " + planet.type +
+           "\nRadius: " + planet.radius + " km" +
+           "\nSize Rank: " + planet.sizeRank + "\n";
+};
+
+// Log details of the first planet
+console.log(getPlanetInfo(planet1));
+
+// Log details of the second planet
+console.log(getPlanetInfo(planet2));
