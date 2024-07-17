e40db58a79b9140d317414550df2803764b05780
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 19:42:01 +0530

    Update Ch3_pgm_01.js

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -23,3 +23,35 @@ console.log(bookTitle + " by " + bookAuthor);
  *    For 100 books.
  *
  */
+
+
+var bookTitle1 = "The Hobbit";
+var bookAuthor1 = "J. R. R. Tolkien";
+
+console.log(bookTitle1 + " by " + bookAuthor1);
+
+// Second Book
+var bookTitle2 = "To Kill a Mockingbird";
+var bookAuthor2 = "Harper Lee";
+
+console.log(bookTitle2 + " by " + bookAuthor2);
+
+// Third Book
+var bookTitle3 = "1984";
+var bookAuthor3 = "George Orwell";
+
+console.log(bookTitle3 + " by " + bookAuthor3);
+
+// Fourth Book
+var bookTitle4 = "Pride and Prejudice";
+var bookAuthor4 = "Jane Austen";
+
+console.log(bookTitle4 + " by " + bookAuthor4);
+
+// Fifth Book
+var bookTitle5 = "The Catcher in the Rye";
+var bookAuthor5 = "J. D. Salinger";
+
+console.log(bookTitle5 + " by " + bookAuthor5);
+
+
