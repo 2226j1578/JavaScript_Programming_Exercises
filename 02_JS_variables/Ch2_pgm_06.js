070268f28a99c337b8874c37878e218218dddcc7
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 19:38:32 +0530

    Update Ch2_pgm_06.js

--- 02_JS_variables/Ch2_pgm_06.js
+++ 02_JS_variables/Ch2_pgm_06.js
@@ -21,3 +21,14 @@ console.log(playerName + " is in " + locationName);
  *    something like "Lionel Messi has a score of 100"
  *
  */
+
+var playerName;
+var locationName;
+var score;
+
+playerName = "Lionel Messi";
+locationName = "Argentina";
+score = 100;
+
+console.log(playerName + " is in " + locationName);
+console.log(playerName + " has a score of " + score);
