9bcbe14fd9c864b79c2465a03f390a49c930ce39
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 21:21:06 +0530

    Update Ch6_pgm_03.js

--- 06_JS_Return_values/Ch6_pgm_03.js
+++ 06_JS_Return_values/Ch6_pgm_03.js
@@ -35,3 +35,15 @@ console.log(getHelloTo("Dax"));
  *    Does it still work?
  *
  */
+
+var getHelloTo;
+
+getHelloTo = function (name) {
+    var template = "Hello to {{name}}";
+    template = template.replace("{{name}}", name);
+    return template;
+};
+
+console.log(getHelloTo("Kandra"));
+console.log(getHelloTo("Dax"));
+
