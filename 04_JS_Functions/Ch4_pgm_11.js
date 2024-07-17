da5da3d7cda59dff9a8687c93874c1c16035b466
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-17 20:45:15 +0530

    Update Ch4_pgm_11.js

--- 04_JS_Functions/Ch4_pgm_11.js
+++ 04_JS_Functions/Ch4_pgm_11.js
@@ -58,3 +58,44 @@ showMovieInfo();
  *    on the console.
  *
  */
+
+var movie1;
+var movie2;
+var movie3;
+var movie;
+var showMovieInfo;
+
+movie1 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+movie2 = {
+    title: "Spectre",
+    actors: "Daniel Craig, Christoph Waltz",
+    directors: "Sam Mendes"
+};
+
+movie3 = {
+    title: "Star Wars: Episode VII - The Force Awakens",
+    actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+    directors: "J.J.Abrams"
+};
+
+showMovieInfo = function () {
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
+};
+
+movie = movie1;
+showMovieInfo();
+
+movie = movie2;
+showMovieInfo();
+
+movie = movie3;
+showMovieInfo();
