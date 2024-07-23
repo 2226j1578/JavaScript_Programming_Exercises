4c096160c521ed0f28b6296567755ef491fcaff6
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:01:59 +0530

    Update Ch8_pgm_01.js

--- 08_JS_Arrays/Ch8_pgm_01.js
+++ 08_JS_Arrays/Ch8_pgm_01.js
@@ -23,3 +23,26 @@ console.log(names);
  * 4) Log akaScores to the console.
  *
  */
+
+// Creating arrays
+
+var scores;
+var names;
+
+scores = [ 3, 1, 8, 2 ];
+names = [ "Kandra", "Dax", "Blinky" ];
+
+console.log(scores);
+console.log(names);
+
+scores.push(5);
+names.push("Frodo");
+
+// 2) Declare a third variable, akaScores
+var akaScores;
+
+// 3) Assign the scores array to akaScores
+akaScores = scores;
+
+// 4) Log akaScores to the console
+console.log(akaScores);
