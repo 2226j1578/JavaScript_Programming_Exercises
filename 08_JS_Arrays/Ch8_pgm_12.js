96ba8b74701453f905819b6fa07a662d227ecf9c
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:09:30 +0530

    Update Ch8_pgm_12.js

--- 08_JS_Arrays/Ch8_pgm_12.js
+++ 08_JS_Arrays/Ch8_pgm_12.js
@@ -39,3 +39,108 @@ var displayQuestion = function (questionAndAnswer) {
    * 4) Can you execute a command at the prompt
    *    to display a particular question?
    */
+
+
+// Displaying a multiple choice question
+
+var displayQuestion = function (questionAndAnswer) {
+    var options = [ "A", "B", "C", "D", "E" ];
+    
+    console.log(questionAndAnswer.question);
+    
+    questionAndAnswer.answers.forEach(
+      function (answer, i) {
+        console.log(options[i] + " - " + answer);
+      }
+    );
+  };
+  
+  var question1 = {
+    question : "What is the capital of France?",
+    answers : [
+      "Bordeaux",
+      "F",
+      "Paris",
+      "Brussels"
+    ],
+    correctAnswer : "Paris"
+  };
+  
+  displayQuestion(question1);
+  
+  var question2 = {
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      "Earth",
+      "Jupiter",
+      "Mars",
+      "Venus"
+    ],
+    correctAnswer: "Jupiter"
+  };
+  
+  var question3 = {
+    question: "What is the smallest unit of life?",
+    answers: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organ"
+    ],
+    correctAnswer: "Cell"
+  };
+
+  var questions = [question1, question2, question3];
+
+  questions.forEach(displayQuestion);
+
+  var displayQuestion = function (questionAndAnswer) {
+    var options = ["A", "B", "C", "D", "E"];
+    
+    console.log(questionAndAnswer.question);
+    
+    questionAndAnswer.answers.forEach(function (answer, i) {
+      console.log(options[i] + " - " + answer);
+    });
+  };
+  
+  var question1 = {
+    question: "What is the capital of France?",
+    answers: [
+      "Bordeaux",
+      "F",
+      "Paris",
+      "Brussels"
+    ],
+    correctAnswer: "Paris"
+  };
+  
+  var question2 = {
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      "Earth",
+      "Jupiter",
+      "Mars",
+      "Venus"
+    ],
+    correctAnswer: "Jupiter"
+  };
+  
+  var question3 = {
+    question: "What is the smallest unit of life?",
+    answers: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organ"
+    ],
+    correctAnswer: "Cell"
+  };
+  
+  var questions = [question1, question2, question3];
+  
+  questions.forEach(displayQuestion);
+  
+  // To display a particular question, you can call:
+  displayQuestion(question2);
+  
