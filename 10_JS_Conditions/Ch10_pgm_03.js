b81f6dadb229a52a619c3b30ddce3715703bd37e
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:17:40 +0530

    Update Ch10_pgm_03.js

--- 10_JS_Conditions/Ch10_pgm_03.js
+++ 10_JS_Conditions/Ch10_pgm_03.js
@@ -48,3 +48,22 @@ var getGuesser = function () {
    * on the console.
    *
    */
+
+// Guess the number - using local scope
+
+var getGuesser = function () {
+    var secret = 8;
+  
+    return function (userNumber) {
+      if (userNumber === secret) {
+        console.log("Well done!");
+      } else {
+        console.log("Unlucky, try again.");
+      }
+    };
+  };
+  
+  var guess = getGuesser();
+  
+  guess(8);  
+guess(5);  
