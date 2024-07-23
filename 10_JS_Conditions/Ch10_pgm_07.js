fcbcb3dc82a83ce2596e23bfa55fe6516734831b
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:19:56 +0530

    Update Ch10_pgm_07.js

--- 10_JS_Conditions/Ch10_pgm_07.js
+++ 10_JS_Conditions/Ch10_pgm_07.js
@@ -102,3 +102,77 @@ var getQuiz = function () {
    *    };
    *
    */
+
+// Checking quiz answers
+
+var getQuiz = function () {
+    var score = 0,
+        qIndex = 0,
+        inPlay = true,
+        questions,
+        next,
+        getQuestion,
+        checkAnswer,
+        submit;
+      
+    questions = [
+      {
+        question: "What is the highest mountain in the world?",
+        answer: "Everest"
+      },
+      {
+        question: "What is the highest mountain in Scotland?",
+        answer: "Ben Nevis"
+      }
+    ];
+    
+    next = function () {
+      qIndex = qIndex + 1;
+      
+      if (qIndex >= questions.length) {
+        inPlay = false;
+        console.log("You have finished the quiz.");
+      }
+    };
+    
+    getQuestion = function () {
+      if (inPlay) {
+        return questions[qIndex].question;
+      } else {
+        return "You have finished the quiz.";
+      }
+    };
+    
+    checkAnswer = function (userAnswer) {
+      if (userAnswer === questions[qIndex].answer) {
+        console.log("Correct!");
+        score = score + 1;
+      } else {
+        console.log("No, the answer is " + questions[qIndex].answer);
+      }
+    };
+    
+    submit = function (userAnswer) {
+      var message = "You have finished the quiz.";
+      
+      if (inPlay) {
+        checkAnswer(userAnswer);
+        next();
+        message = "Your score is " + score + " out of " + qIndex;
+      }
+        
+      return message;
+    };
+    
+    return {
+      quizMe: getQuestion,
+      submit: submit
+    };
+  };
+  
+  var quiz = getQuiz();
+  
+  console.log(quiz.quizMe());  
+console.log(quiz.submit("Everest"));  
+
+  
