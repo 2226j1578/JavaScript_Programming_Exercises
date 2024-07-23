c61ec2e32ac9a7b0a899097c9d853a04d1954ac7
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:14:09 +0530

    Update Ch9_pgm_06.js

--- 09_JS_Constructors/Ch9_pgm_06.js
+++ 09_JS_Constructors/Ch9_pgm_06.js
@@ -52,3 +52,48 @@ var planet3 = new Planet("Mercury", 1, "Terrestrial");
  *      Europa
  *
  */
+
+
+var Planet = function (name, position, type) {
+    this.name = name;
+    this.position = position;
+    this.type = type;
+    this.moons = [];
+  
+    this.showMoons = function () {
+        this.moons.forEach(function(moon, index) {
+            console.log("(" + index + ") " + moon);
+        });
+    };
+  
+    this.showPlanet = function () {
+        console.log(this.name);
+        console.log("Planet " + this.position + " - " + this.type);
+        console.log("Moons:");
+        this.showMoons();
+    };
+  
+    this.addMoon = function (moon) {
+        this.moons.unshift(moon);
+    };
+  
+    this.getMoon = function (index) {
+        return this.moons[index];
+    };
+};
+
+var planet1 = new Planet("Jupiter", 5, "Gas Giant");
+planet1.addMoon("Io");
+planet1.addMoon("Europa");
+
+var planet2 = new Planet("Neptune", 8, "Ice Giant");
+planet2.addMoon("Triton");
+
+var planet3 = new Planet("Mercury", 1, "Terrestrial");
+
+[ planet1, planet2, planet3 ].forEach(function (planet) {
+    planet.showPlanet();
+});
+
+console.log(planet1.getMoon(1));
+  
