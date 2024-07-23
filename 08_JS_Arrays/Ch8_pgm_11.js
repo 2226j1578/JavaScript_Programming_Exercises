0a27d028d99ae5212eef1f4466efa913e7f67a0d
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:08:28 +0530

    Update Ch8_pgm_11.js

--- 08_JS_Arrays/Ch8_pgm_11.js
+++ 08_JS_Arrays/Ch8_pgm_11.js
@@ -35,3 +35,62 @@ console.log("The total cost is $" + getTotalBill(costs, numOfEach));
  *    of items.
  *
  */
+
+
+// Finding the total shopping bill
+
+var getTotalBill = function (itemCosts, itemCounts) {
+	var total = 0;
+
+	itemCosts.forEach(function (cost, i) {
+		total += cost * itemCounts[i];
+	});
+
+	return total;
+};
+
+var costs = [ 1.99, 4.95, 2.50, 9.87 ];
+var numOfEach = [ 2, 1, 5, 2 ];
+
+console.log("The total cost is $" + getTotalBill(costs, numOfEach));
+
+var items = [
+	{ cost: 1.99, numberBought: 2 },
+	{ cost: 4.95, numberBought: 1 },
+	{ cost: 2.50, numberBought: 5 },
+	{ cost: 9.87, numberBought: 2 },
+	{ cost: 3.75, numberBought: 3 }
+  ];
+
+  var getTotalBill = function (items) {
+	var total = 0;
+  
+	items.forEach(function (item) {
+	  total += item.cost * item.numberBought;
+	});
+  
+	return total;
+  };
+  
+  console.log("The total cost is $" + getTotalBill(items));
+
+  var items = [
+	{ cost: 1.99, numberBought: 2 },
+	{ cost: 4.95, numberBought: 1 },
+	{ cost: 2.50, numberBought: 5 },
+	{ cost: 9.87, numberBought: 2 },
+	{ cost: 3.75, numberBought: 3 }
+  ];
+  
+  var getTotalBill = function (items) {
+	var total = 0;
+  
+	items.forEach(function (item) {
+	  total += item.cost * item.numberBought;
+	});
+  
+	return total;
+  };
+  
+  console.log("The total cost is $" + getTotalBill(items));
+  
