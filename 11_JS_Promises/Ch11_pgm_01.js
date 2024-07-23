92c49ae7bf207d32da62ca4d9b7951dfe5d2a273
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:23:34 +0530

    Update Ch11_pgm_01.js

--- 11_JS_Promises/Ch11_pgm_01.js
+++ 11_JS_Promises/Ch11_pgm_01.js
@@ -20,3 +20,22 @@ const doSomething = callback => {
 // after 2 seconds it will print
 // It did not go well
 
+// Callbacks
+
+const doSomething = callback => {
+  setTimeout(() => {
+    const skills = ['HTML', 'CSS', 'JS']
+    callback('It did not go well', skills)
+  }, 2000)
+}
+
+const callback = (err, result) => {
+  if (err) {
+      return console.log(err)
+  }
+  return console.log(result)
+}
+
+doSomething(callback)
+
+
