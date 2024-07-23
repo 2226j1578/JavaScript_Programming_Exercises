87a6237041d23b018492e1a1a404465b2ee4f521
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:12:17 +0530

    Update Ch9_pgm_03.js

--- 09_JS_Constructors/Ch9_pgm_03.js
+++ 09_JS_Constructors/Ch9_pgm_03.js
@@ -39,3 +39,40 @@ planets.forEach(function (planet) {
  *    from the next.
  *
  */
+
+
+// An array of constructed objects
+
+var buildPlanet = function (name, position, type) {
+    var planet = {};
+  
+    planet.name = name;
+    planet.position = position;
+    planet.type = type;
+
+    planet.showPlanet = function () {
+        var info = planet.name;
+        info += ": planet " + planet.position;
+        info += " - " + planet.type;
+        console.log(info);
+      };
+
+    return planet;
+};
+
+var planets = [
+    buildPlanet( "Jupiter", 5, "Gas Giant" ),
+    buildPlanet( "Neptune", 8, "Ice Giant" ),
+    buildPlanet( "Mercury", 1, "Terrestrial" )
+];
+
+planets.forEach(function (planet) {
+    planet.showPlanet();
+});
+
+planets.forEach(function (planet, index) {
+    planet.showPlanet();
+    if (index < planets.length - 1) {
+        console.log("--------------------");
+    }
+});
