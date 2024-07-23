c10078577524f9977d1eaea49a586434f542a14d
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:14:43 +0530

    Update Ch9_pgm_07.js

--- 09_JS_Constructors/Ch9_pgm_07.js
+++ 09_JS_Constructors/Ch9_pgm_07.js
@@ -46,3 +46,45 @@ question1.showQuestion();
  *    Press Enter
  *
  */
+
+
+// A quiz question constructor
+
+var QuizQuestion = function (question, answer) {
+    this.question = question;
+    this.answer = answer;
+    this.options = [];
+  
+    this.addOption = function (option) {
+        this.options.push(option);
+    };
+  
+    this.showQuestion = function () {
+        console.log(this.question);
+        this.options.forEach(function (option, i) {
+            console.log("(" + (i + 1) + ") " + option);
+        });
+    };
+};
+
+var question1 = new QuizQuestion(
+    "What is the capital of France?",
+    "Paris"
+);
+
+question1.addOption("Bordeaux");
+question1.addOption("F");
+question1.addOption("Paris");
+question1.addOption("Brussels");
+
+question1.showQuestion();
+
+var question2 = new QuizQuestion(
+    "Which planet is known as the Red Planet?",
+    "Mars"
+);
+
+question2.addOption("Earth");
+question2.addOption("Mars");
+question2.addOption("Jupiter");
+question2.addOption("Saturn");
