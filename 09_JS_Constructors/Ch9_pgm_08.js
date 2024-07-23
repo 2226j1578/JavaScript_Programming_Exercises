ea89dbb90782b0c23d774e03a52fe48b9359217e
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:15:13 +0530

    Update Ch9_pgm_08.js

--- 09_JS_Constructors/Ch9_pgm_08.js
+++ 09_JS_Constructors/Ch9_pgm_08.js
@@ -43,3 +43,44 @@ calEvent.showEvent();
  *    Annual Review: 3/5/16 - (4.00pm - 5.00pm)
  *
  */
+
+// A calendar event constructor
+
+var CalendarEvent = function (title, startDate, startTime, endTime) {
+    this.title = title;
+    this.startDate = startDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  
+    this.showEvent = function () {
+        var dateString = [
+            this.startDate,
+            ", from ",
+            this.startTime,
+            " to ",
+            this.endTime
+        ].join("");
+      
+        console.log(this.title);
+        console.log(dateString);
+    };
+};
+
+var calEvent = new CalendarEvent(
+    "Annual Review",
+    "3/5/16",
+    "4.00pm",
+    "5.00pm"
+);
+
+calEvent.showEvent();
+
+var calEvent2 = new CalendarEvent(
+    "Team Meeting",
+    "3/6/16",
+    "10.30am",
+    "11.30am"
+);
+
+// 2) Call the showEvent method on your new calendar event
+calEvent2.showEvent();
