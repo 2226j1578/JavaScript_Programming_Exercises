8e185d3dc1a86135fc8b1eea19c04a40f4c8942e
2226j1578 <144420397+2226j1578@users.noreply.github.com>
2024-07-23 21:16:39 +0530

    Update Ch10_pgm_01.js

--- 10_JS_Conditions/Ch10_pgm_01.js
+++ 10_JS_Conditions/Ch10_pgm_01.js
@@ -35,3 +35,25 @@ var guess = function (userNumber) {
  * 6) Run and test the updated program.
  *
  */
+
+
+// Guess the number
+
+var secret = 8;
+
+var guess = function (userNumber) {
+  if (userNumber === secret) {
+    console.log("Well done!");
+  }
+};
+
+var secret = 8;
+
+var guess = function (userNumber) {
+  if (userNumber > secret) {
+    console.log("Too high!");
+  }
+};
+
+guess(9);
+guess(7);
